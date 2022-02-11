41e68f6eb0acf32aee0e8c064e7b0c885fa6aeba
Blake Whitman <whitman.124@osu.edu>
2022-02-11 12:02:15 -0500

    Create config.py

---
+++ script/config.py
@@ -0,0 +1,18 @@
+# Base
+URL = 'SCHOOL_URL'
+
+# Course
+COURSE = 'YOUR_COURSE_ID' # your course's ID
+
+# Login Credentials
+USERNAME = 'YOUR_USERNAME' # your uni username (e.g whitman.124)
+PASSWORD = 'YOUR_PASSWORD' # your uni password
+
+# Lab Info
+DATE = {
+  'day': 'DESIRED_DAY', # your desired day (e.g. Tuesday)
+  'time': 'DESIRED_HOUR' # your desired hour (e.g. 19:00)
+}
+
+# Interval (in minutes)
+INTERVAL = 3
